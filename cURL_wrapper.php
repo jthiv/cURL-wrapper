032903c08f6454fc58148d625621a24f75fd511a
jthiv <jthintoniv@gmail.com>
2015-01-08 15:56:41 -0500

    Removed PoC
    PoC was moved to readme file

--- cURL_wrapper.php
+++ cURL_wrapper.php
@@ -76,47 +76,4 @@ class cURL_wrapper{
 	    
     }
 }
-/********************************************************
-PROOF OF CONCEPT
-
-I will use the Sunlight Foundation Congress API. View
-documentation on this API here:
-https://sunlightlabs.github.io/congress/
-
-You will need a valid API key for this to work.
----------------------------------------------------------
-
-$url_base = "https://congress.api.sunlightfoundation.com";
-$api_key = "***************";
-$method ="/legislators";
-$params = array();
-    $params[0]=array("name" => "state",
-		     "value" => "VA"
-		    );
-    $params[1]=array("name" => "chamber",
-		     "value" => "senate"
-		    );
-    $params[2]=array("name" => "in_office",
-		     "value" => "true"
-		    );
-$objAPI = new cURL_wrapper($method,$params,$url_base,$api_key);
-try
-{
-    if($objAPI)
-    {
-	$jsonResponse = $objAPI->getMethodResponse();
-	$arrResponse = json_decode($jsonResponse,true);
-    }
-    else
-    {
-	throw new Exception("API Error: Invalid response.");
-    }
-}
-catch(Exception $e)
-{
-    echo $e->getMessage();
-}
-********************************************************/
-
-
 ?>
